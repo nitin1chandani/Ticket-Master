05f8501b8258e3c5bfe851124d12618e486716b4
nitin1chandani <nitin1chandani>
2026-09-20 16:01:09 +0530

    feat: reorganize route registration for better structure and clarity

--- app/routes.go
+++ app/routes.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/nitin1chandani/ticketmaster/auth"
+	"github.com/nitin1chandani/ticketmaster/bookings"
+	"github.com/nitin1chandani/ticketmaster/event"
 	"github.com/nitin1chandani/ticketmaster/internal/middleware"
+	"github.com/nitin1chandani/ticketmaster/user"
 )
 
 func (c *Container) registerRoutes() {
@@ -17,9 +20,25 @@ func (c *Container) registerRoutes() {
 
 	authMW := middleware.NewAuthMiddleware(c.Config.JWTSecret)
 
+	//auth
 	authRepo := auth.NewRepository(c.DB)
 	authService := auth.NewService(authRepo, c.Config.JWTSecret, time.Duration(c.Config.JWTExpiryHours)*time.Hour)
 	authHandler := auth.NewHandler(authService)
+
+	//user
+	userRepo := user.NewRepository(c.DB)
+	userService := user.NewService(userRepo)
+	userHandler := user.NewUserHandler(userService)
+
+	//booking
+	bookingRepo := bookings.NewBookingRepo(c.DB)
+	bookingService := bookings.NewBookingService(bookingRepo)
+	bookingHandler := bookings.NewBookingHandler(bookingService)
+
+	// event
+	eventRepo := event.NewEventRepo(c.DB)
+	eventService := event.NewEventService(eventRepo)
+	eventHandler := event.NewEventHandler(eventService)
 
 	api := c.App.Group("/api")
 	v1 := api.Group("/v1")
@@ -28,8 +47,19 @@ func (c *Container) registerRoutes() {
 	authV1 := v1.Group("/auth")
 	authV1.Post("/login", authHandler.Login)
 
+	// user
+	userV1 := v1.Group("/user")
+	userV1.Post("/register", userHandler.Register)
+
 	//protected
 	protectedV1 := v1.Group("/", authMW.RequireJWT)
 	protectedV1.Get("/me", authHandler.Me)
+
+	bookingV1 := protectedV1.Group("/booking")
+	bookingV1.Post("/:event_id", bookingHandler.BookTickets)
+
+	eventV1 := protectedV1.Group("/event")
+	eventV1.Post("/", eventHandler.CreateNewEvent) // only admin can create we need a middleware
+	eventV1.Get("/:event_id", eventHandler.GetEventDetailsWithAvailableTickets)
 
 }
