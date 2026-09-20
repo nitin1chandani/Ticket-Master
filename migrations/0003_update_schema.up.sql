5a52509d244edaf79a7f42dc50357ce09483b734
nitin1chandani <nitin1chandani>
2026-09-20 19:37:21 +0530

    feat: update database schema to add status column in bookings and reserved_until in tickets

--- migrations/0003_update_schema.up.sql
+++ migrations/0003_update_schema.up.sql
@@ -1,10 +1,9 @@
 ALTER TABLE tickets
-ADD COLUMN reserved_until TIMESTAMPTZ
+ADD COLUMN reserved_until TIMESTAMPTZ;
 
 ALTER TABLE bookings
-ADD COLUMN status VARCHAR(20) NOT NULL DEFAULE 'pending'
+ADD COLUMN status VARCHAR(20) NOT NULL DEFAULT 'pending';
 
 ALTER TABLE bookings
 ADD CONSTRAINT booking_status_check
-CHECK (status IN ('pending', 'confirmed', 'expired', 'cancelled'))
-
+CHECK (status IN ('pending', 'confirmed', 'expired', 'cancelled'));
--- user/repository.go
+++ user/repository.go
@@ -37,7 +37,7 @@ func NewRepository(db *pgxpool.Pool) *Repository {
 func (r *Repository) GetRoleIDByName(ctx context.Context, roleName string) (int, error) {
 	q := "SELECT id FROM roles WHERE name = $1 LIMIT 1"
 	var id int
-	err := r.db.QueryRow(ctx, q).Scan(&id)
+	err := r.db.QueryRow(ctx, q, roleName).Scan(&id)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return 0, ErrUserRoleNotFound
@@ -49,14 +49,14 @@ func (r *Repository) GetRoleIDByName(ctx context.Context, roleName string) (int,
 
 func (r *Repository) CreateUser(ctx context.Context, roleID int, p CreateUserParams) (*User, error) {
 	q := "" +
-		"INSERT INTO users (name, email, username, role_id, password_hash, created_at)" +
-		"VALUES ($1, $2, $3, $4, $5)" +
+		"INSERT INTO users (name, email, username, role_id, password_hash, created_by, created_at) " +
+		"VALUES ($1, $2, $3, $4, $5, $6, $7) " +
 		"RETURNING id, name, email, username, role_id, created_at"
 
 	u := &User{}
 	err := r.db.QueryRow(
 		ctx, q,
-		p.Name, p.Email, p.Username, p.RoleID, p.PasswordHash, time.Now(),
+		p.Name, p.Email, p.Username, roleID, p.PasswordHash, "self_registration", time.Now(),
 	).Scan(&u.ID, &u.Name, &u.Email, &u.Username, &u.RoleID, &u.CreatedAt)
 
 	if err != nil {
